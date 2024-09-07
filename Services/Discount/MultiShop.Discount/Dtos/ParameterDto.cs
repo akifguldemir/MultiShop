c7127de296d251aa90b9df9744b8e90461a58f24
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-07 14:57:03 +0300

    Discount Services is completed

---
+++ Services/Discount/MultiShop.Discount/Dtos/ParameterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Discount.Dtos
+{
+    public class ParameterDto
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/Services/IDiscountServices.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
+{
+    public interface IDiscountServices
+    {
+        Task<List<ResultCouponDto>> GetAllCouponAsync();
+        Task CreateCouponAsync(CreateCouponDto createCouponDto);
+        Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
+        Task DeleteCouponAsync(int id);
+        Task<GetByIdCouponDto> GetByIdCouponAsync(int id);
+    }
+}
