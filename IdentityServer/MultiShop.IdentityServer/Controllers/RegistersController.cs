77e3febfbab75248e165ed740c23aee00e47a39e
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-09 15:39:55 +0300

    IdentityServer implemeted

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname
+            };
+            var result = await userManager.CreateAsync(values, userRegisterDto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok("User created");
+            }
+            else
+            {
+                return Ok("User cannot created!");
+            }
+        }
+    }
+}
