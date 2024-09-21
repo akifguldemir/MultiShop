c9c37885bc318da9102645abe5fc77b7bce67a59
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-21 15:37:39 +0300

    ProductList process

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListPriceSizeFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProoductListViewComponents
+{
+    public class _ProductListPriceSizeFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
