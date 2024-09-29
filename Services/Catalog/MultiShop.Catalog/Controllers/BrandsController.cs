719dac77e29ee4361d48b20584195931de4f727c
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-29 23:03:50 +0300

    Brand api is completed

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/BrandsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.BrandDtos;
+using MultiShop.Catalog.Services.BrandServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _BrandService;
+
+        public BrandsController(IBrandService BrandService)
+        {
+            _BrandService = BrandService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BrandList()
+        {
+            var values = await _BrandService.GetAllBrandAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrandById(string id)
+        {
+            var values = await _BrandService.GetByIdBrandAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBrand(CreateBrandDto createBrandDto)
+        {
+            await _BrandService.CreateBrandAsync(createBrandDto);
+            return Ok("Brand added");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBrand(string id)
+        {
+            await _BrandService.DeleteBrandAsync(id);
+            return Ok("Brand deleted");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateBrand(UpdateBrandDto updateBrandDto)
+        {
+            await _BrandService.UpdateBrandAsync(updateBrandDto);
+            return Ok("Brand updated");
+        }
+    }
+}
