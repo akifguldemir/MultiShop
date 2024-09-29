dc24799cb7765742c78c5f4a56681772b366565c
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-29 23:20:38 +0300

    About api is completed

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/AboutsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.AboutDto;
+using MultiShop.Catalog.Services.AboutServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutsController : ControllerBase
+    {
+        private readonly IAboutService _AboutService;
+
+        public AboutsController(IAboutService AboutService)
+        {
+            _AboutService = AboutService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutList()
+        {
+            var values = await _AboutService.GetAllAboutAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAboutById(string id)
+        {
+            var values = await _AboutService.GetByIdAboutAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        {
+            await _AboutService.CreateAboutAsync(createAboutDto);
+            return Ok("About added");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAbout(string id)
+        {
+            await _AboutService.DeleteAboutAsync(id);
+            return Ok("About deleted");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            await _AboutService.UpdateAboutAsync(updateAboutDto);
+            return Ok("About updated");
+        }
+    }
+}
