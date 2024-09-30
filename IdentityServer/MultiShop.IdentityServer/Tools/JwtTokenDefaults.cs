0daf9a5dae2bff7f1fc702254972eb50246f8e73
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-30 14:59:20 +0300

    token generated after succesfull login

---
+++ IdentityServer/MultiShop.IdentityServer/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.IdentityServer.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string Key = "MultiShop+*010203MULTISHOP01+*..020304MultiShopProje";
+        public const int Expire = 60;
+    }
+}
---
+++ IdentityServer/MultiShop.IdentityServer/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+
+namespace MultiShop.IdentityServer.Tools
+{
+    public class JwtTokenGenerator
+    {
+        public static TokenResponseViewModel GenerateToken(GetCheckAppUserViewModel result)
+        {
+            var claims = new List<Claim>();
+
+            if (!string.IsNullOrWhiteSpace(result.Role))
+                claims.Add(new Claim(ClaimTypes.Role, result.Role));
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, result.Id.ToString()));
+
+            if (!string.IsNullOrWhiteSpace(result.Username))
+                claims.Add(new Claim("Username", result.Username));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key));
+
+            var signinCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);
+
+            JwtSecurityToken token = new JwtSecurityToken(issuer: JwtTokenDefaults.ValidIssuer, audience: JwtTokenDefaults.ValidAudience, claims: claims, notBefore: DateTime.UtcNow, expires: expireDate, signingCredentials: signinCredentials);
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            return new TokenResponseViewModel(tokenHandler.WriteToken(token), expireDate);
+        }
+    }
+}
